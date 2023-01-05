1a017768f05f7bd3b115afac2c92206b6b4c7fd3
alfanridho <muha22092ti@nurulfikri.ac.id>
2023-01-05 07:54:24 +0700

    pasang owlcarousel di bagian dokter

---
+++ assets/owlcarousel.js
@@ -0,0 +1,9 @@
+$(".owl-carousel").owlCarousel({
+  loop: true, // agar carousel terus berputar
+  margin: 10, // jarak antara item
+  nav: true, // menambahkan navigasi
+  dots: false, // menghilangkan dots navigasi
+  autoplay: true, // mengaktifkan autoplay
+  autoplayTimeout: 3000, // waktu antara pergantian item (dalam milisecond)
+  autoplayHoverPause: true, // menghentikan autoplay saat mouse diarahkan ke carousel
+});
